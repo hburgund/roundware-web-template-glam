437bb43e25c1977a0d46976872b369afe1d608cb
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-19 18:30:18 +0530

    play code from context

--- src/components/App/ConcludeButton.tsx
+++ src/components/App/ConcludeButton.tsx
@@ -4,19 +4,18 @@ import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { useHistory } from 'react-router-dom';
 import Backdrop from '@mui/material/Backdrop';
 import { useRoundware } from 'hooks';
-import codaMp3 from 'assets/coda.mp3';
+
 import config from 'config.json';
 interface Props {}
 const duration = config.CONCLUDE_DURATION;
 const ConcludeButton = (props: Props) => {
 	const history = useHistory();
-	const { roundware } = useRoundware();
+	const { roundware, codaAudio } = useRoundware();
 	const [conclude, setConclude] = useState(false);
 
-	const codeAudio = useMemo(() => new Audio(codaMp3), []);
 	const handleClick = () => {
 		setConclude(true);
-		codeAudio.play();
+		codaAudio.play();
 		roundware.mixer?.playlist?.tracks.forEach((t) => t.fadeOut(duration));
 		roundware.mixer?.speakerTracks?.forEach((s) => s.player.fade(0, duration));
 		setTimeout(() => {
