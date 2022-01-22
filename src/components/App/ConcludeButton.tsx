0ac6fac27c99ea42930ab1e9969415e008e3d75f
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2022-01-22 23:40:08 +0530

    auto conclude

--- src/components/App/ConcludeButton.tsx
+++ src/components/App/ConcludeButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { useHistory } from 'react-router-dom';
@@ -12,6 +12,20 @@ const ConcludeButton = (props: Props) => {
 	const history = useHistory();
 	const { roundware, codaAudio } = useRoundware();
 	const [conclude, setConclude] = useState(false);
+
+	useEffect(() => {
+		if (config.AUTO_CONCLUDE_DURATION) {
+			setTimeout(() => {
+				setConclude(true);
+				roundware.mixer?.playlist?.tracks.forEach((t) => t.fadeOut(duration));
+				roundware.mixer?.speakerTracks?.forEach((s) => s.player.fade(0, duration));
+			}, config.AUTO_CONCLUDE_DURATION * 1000);
+			setTimeout(() => {
+				history.push(`/conclusion`);
+				roundware.mixer.stop();
+			}, (config.AUTO_CONCLUDE_DURATION + duration) * 1000);
+		}
+	}, []);
 
 	const handleClick = () => {
 		setConclude(true);
